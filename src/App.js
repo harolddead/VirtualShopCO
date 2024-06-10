3b3eead6e36692d7f0fae2040a0b9478558668dd
harolddead <135444888+harolddead@users.noreply.github.com>
2024-06-10 18:38:03 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,6 +7,25 @@ import adidas from './assets/img/adidas.jpg';
 // Importa el archivo de estilos CSS
 import './App.css';
 
+  // Codigo Buscador
+
+var busqueda = document.getElementById("buscar");
+var lupa = document.getElementById("lupa");
+lupa.style.left = (parseInt(busqueda.offsetLeft)+39.5)+"%";
+lupa.style.top = (parseInt(busqueda.offsetTop)+9)+"%";
+
+var busqueda = document.getElementById("buscar");
+var lupa = document.getElementById("lupa");
+
+
+// Codigo Buscador
+
+// Actualiza la posición del icono si el tamaño del input cambia
+busqueda.addEventListener("input", function() {
+  lupa.style.right = "10px"; // Ajusta la posición según sea necesario
+});
+
+
 // Define el componente funcional App
 const App = (_) => {
   return (
@@ -16,6 +35,8 @@ const App = (_) => {
       <img src={adidas} />
     </div>
   );
+
+
 };
 
 // Exporta el componente App como el export por defecto
